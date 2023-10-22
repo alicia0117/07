9a8f16401eefa0851c8cd62cb23ead462ad9a5cc
alicia0117 <alicia0117@naver.com>
2023-10-22 21:14:38 +0900

    실습 2

--- main.c
+++ main.c
@@ -3,16 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-  int flag = 1;
-  int y; 
-  while( flag != 0 )
+  int i;
+  int temp = 1;
+  
+  for( i=0;i<5;i++ )
   {
-         int y;
-         y= 3;
-         flag = 0;
-         } 
-  y = 4;
+       printf("temp = %d\n", temp);
+       temp++;
+       } 
   
   system("PAUSE");	
   return 0;
-}
+} 
